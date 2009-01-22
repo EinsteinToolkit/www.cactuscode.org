9eaa26c637dd80502ca47cdee2bd7f6a6daf8db6
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-22 19:09:51 +0000

    modified update web interface

--- x.php
+++ x.php
@@ -6,17 +6,18 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 echo '<br />';
 if(isset($_GET['golive']))
 {
-  echo exec('/var/www/golive');
+  echo '<p>'.exec('/var/www/golive').'</p>';
   echo '<p>Live site updated!</p>';
 }
-elseif(isset($_GET['getpreview']))
+elseif(isset($_GET['updatesvn']))
 {
-  echo exec('/var/www/updatesvn');
+  echo '<p>'.exec('/var/www/updatesvn').'</p>';
   echo '<p>Preview site updated.</p>';
 }
 else
 {
-  echo '<p><h3><a href="/x?golive">Update the live site now</a></h3></p>';
+  echo '<p><h3><a href="/x?updatesvn">Update the preview site now</a></h3></p>
+  <p><h3><a href="/x?golive">Push changes to the live site now</a></h3></p>';
 }
 
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
