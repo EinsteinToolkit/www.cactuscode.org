1e4044ff08b811b2e21069cd31a53894b1260930
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-22 23:27:07 +0000

    add several new members

--- about/team/index.php
+++ about/team/index.php
@@ -11,36 +11,56 @@ team:
 </p>
 
 <ul>
-<li><a href="gallen">Gabrielle Allen</a>
+<li><a href="gallen.php">Gabrielle Allen</a>
 <br />Lead of Cactus project at <a href="http://lsu.edu/">LSU</a>, and inactive developer</li>
 <br />
 
-<li><a href="yelkhamra">Yaakoub El-Khamra</a>
-<br />Research scientist at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://lsu.edu/">LSU</a></li>
+<li><a href="ebentivegna.php">Eloisa Bentivegna</a>
+<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://lsu.edu/">LSU</a></li>
 <br />
 
-<li><a href="tgoodale">Tom Goodale</a>
+<li><a href="yelkhamra.php">Yaakoub El-Khamra</a>
+<br />Research Engineering / Scientist Associate III at <a href="http://www.tacc.utexas.edu/">Texas Advanced Computing Center</a></li>
+<br />
+
+<li><a href="tgoodale.php">Tom Goodale</a>
 <br />Research scientist at both <a href="http://lsu.edu/">LSU</a> and Cardiff University.</li>
 <br />
 
-<li><a href="tradke">Thomas Radke</a>
+<li><a href="sko.php">Soon-Heum Ko</a>
+<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://lsu.edu/">LSU</a></li>
+<br />
+
+<li><a href="floeffler.php">Frank Loeffler</a>
+<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://lsu.edu/">LSU</a></li>
+<br />
+
+<li><a href="tradke.php">Thomas Radke</a>
 <br />Scientific Programmer and Lead of the e-Science group in the <a href="http://www.aei.mpg.de/english/research/teams/astrophysicalRelativity/">Astrophysical Relativity Division</a>
 of the <a href="http://www.aei.mpg.de/" title="Max-Planck-Institute for Gravitational Physics (Albert Einstein Institute)">AEI</a></li>
 <br />
 
-<li><a href="eschnetter">Erik Schnetter</a>
+<li><a href="drideout.php">David Rideout</a>
+<br />Researcher in the <a href="http://www.perimeterinstitute.ca/en/Scientific/Research/Quantum_Foundations/">Quantum  Foundations Group</a> at the <a href="http://www.perimeterinstitute.ca/">Perimeter Institute for Theoretical Physics</a></li>
+<br />
+
+<li><a href="eschnetter.php">Erik Schnetter</a>
 <br />Scientist in the <a href="http://www.cct.lsu.edu/about/focus/numerical/index.php">Numerical Relativity Group</a> at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a></li>
 <br />
 
-<li><a href="jtao">Jian Tao</a>
+<li><a href="jtao.php">Jian Tao</a>
 <br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://lsu.edu/">LSU</a></li>
 <br />
 
-<li><a href="ecaraba">Elena Caraba</a>
+<li><a href="okorobkin.php">Oleg Korobkin</a>
+<br />Graduate student at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://lsu.edu/">LSU</a></li>
+<br />
+
+<li><a href="ecaraba.php">Elena Caraba</a>
 <br />Research Undergraduate Student at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a></li>
 <br />
 
-<li><a href="cjordan">Colby Jordan</a>
+<li><a href="cjordan.php">Colby Jordan</a>
 <br />Internal Support for Cactus at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a></li>
 <br />
 </ul>
