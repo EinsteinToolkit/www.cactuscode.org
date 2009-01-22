f19db733231b85ac4c73b6ae5e39dc852400c4f1
eschnett <eschnett@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-22 22:01:13 +0000

    Add missing </p>

--- about/funding.php
+++ about/funding.php
@@ -5,64 +5,64 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
   Cactus has been made possible by a variety of grants and funding
   from different institutions.  In particular we would like to give
   thanks to
-  
-  <ul>
-    <li> Max-Planck-Gesellschaft </li>
-    <li> Center for Computational &amp; Technology at LSU </li>
-    <li> National Center for Supercomputing Applications </li>
-    <li> Lawrence Berkeley National Laboratory </li>
-    <li> Washington University </li>
-    <li> University of Tübingen </li>
-  </ul>
 </p>
+  
+<ul>
+  <li> Max-Planck-Gesellschaft </li>
+  <li> Center for Computational &amp; Technology at LSU </li>
+  <li> National Center for Supercomputing Applications </li>
+  <li> Lawrence Berkeley National Laboratory </li>
+  <li> Washington University </li>
+  <li> University of Tübingen </li>
+</ul>
 
 <p>
   Currently active grants supporting Cactus:
-  
-  <ul>
-    <li> NSF <a href="http://www.cybertools.loni.org"><i>Louisiana's
-          Research Infrastructure Improvement Strategy
-          (CyberTools)</i></a> (NSF 0701491) </li>
-    <li> NSF <a href="http://www.cct.lsu.edu/xirel/"><i>XiRel: Next
-          Generation Infrastructure for Numerical Relativity</i></a>
-          (NSF 0701566 and 0653303) </li>
-    <li>
-      NSF <a href="http://www.cct.lsu.edu/~eschnett/Alpaca/"><i>Alpaca:
-          Cactus Tools for Application Level Profiling and Correctness
-          Analysis</i></a> (NSF 0701566) </li>
-    <li> ONR <i>Development of an Integrated Modeling Framework for
-        Simulations of Coastal Processes in Deltaic Environments Using
-        High-Performance Computing (COMI)</i> </li>
-    <li> DOE/BOR <i>Ubiquitous Computing and Monitoring System for
-        Discovery and Management of Energy Resources (UCOMS)</i> (DOE
-      DE-FG02-04ER46136, BOR DOE/LEQSF(2004-07)) </li>
-  </ul>
 </p>
+  
+<ul>
+  <li> NSF <a href="http://www.cybertools.loni.org"><i>Louisiana's
+        Research Infrastructure Improvement Strategy
+        (CyberTools)</i></a> (NSF 0701491) </li>
+  <li> NSF <a href="http://www.cct.lsu.edu/xirel/"><i>XiRel: Next
+        Generation Infrastructure for Numerical Relativity</i></a>
+    (NSF 0701566 and 0653303) </li>
+  <li>
+    NSF <a href="http://www.cct.lsu.edu/~eschnett/Alpaca/"><i>Alpaca:
+        Cactus Tools for Application Level Profiling and Correctness
+        Analysis</i></a> (NSF 0701566) </li>
+  <li> ONR <i>Development of an Integrated Modeling Framework for
+      Simulations of Coastal Processes in Deltaic Environments Using
+      High-Performance Computing (COMI)</i> </li>
+  <li> DOE/BOR <i>Ubiquitous Computing and Monitoring System for
+      Discovery and Management of Energy Resources (UCOMS)</i> (DOE
+      DE-FG02-04ER46136, BOR DOE/LEQSF(2004-07)) </li>
+</ul>
 
 <p>
   Cactus has in the past been supported through the following grants:
-  
-  <ul>
-    <li> NSF <i>Astrophysics Simulation Collaboratory: Enabling Large
-        Scale Simulations of Relativistic Astrophysics</i> (NSF
-        PHY-9979985) </li>
-    <li> European Commission <i>Theoretical Foundations of Sources of
-        Gravitational Waves in the Next Century: Synergy between
-        Supercomputer Simulations and Approximation Techniques</i>
-        (HPRN-CT-2000-00137) </li>
-    <li> DFN-Verein <i>GriKSL: Grid-Immersion: Kollision Schwarzer
-        Löcher</i> (TK 6-2-AN 200) </li>
-    <li> European Commission <i>GridLab: A Grid Application Toolkit
-        and Testbed</i> (IST-2001-32133) </li>
-    <li> DFG <i>Gravitational Wave Astronomy and Sources of
-        Gravitational Waves</i> (SRB/TR 7) </li>
-    <li> DFN-Verein <i>TiKSL: Tele Immersion: Collision of Black
-        Holes</i> </li>
-    <li>
-      DDDAS-TMRP <i><a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0540374">DynaCode:
-          A General DDDAS Framework with Coast and Environment
-          Modeling Applications</a></i> </li>
-  </ul>
 </p>
+  
+<ul>
+  <li> NSF <i>Astrophysics Simulation Collaboratory: Enabling Large
+      Scale Simulations of Relativistic Astrophysics</i> (NSF
+      PHY-9979985) </li>
+  <li> European Commission <i>Theoretical Foundations of Sources of
+      Gravitational Waves in the Next Century: Synergy between
+      Supercomputer Simulations and Approximation Techniques</i>
+      (HPRN-CT-2000-00137) </li>
+  <li> DFN-Verein <i>GriKSL: Grid-Immersion: Kollision Schwarzer
+      Löcher</i> (TK 6-2-AN 200) </li>
+  <li> European Commission <i>GridLab: A Grid Application Toolkit and
+      Testbed</i> (IST-2001-32133) </li>
+  <li> DFG <i>Gravitational Wave Astronomy and Sources of
+      Gravitational Waves</i> (SRB/TR 7) </li>
+  <li> DFN-Verein <i>TiKSL: Tele Immersion: Collision of Black
+      Holes</i> </li>
+  <li>
+    DDDAS-TMRP <i><a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0540374">DynaCode:
+        A General DDDAS Framework with Coast and Environment Modeling
+        Applications</a></i> </li>
+</ul>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
