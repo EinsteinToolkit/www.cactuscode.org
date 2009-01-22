0f11d14c4f0f086b051ae527986613a5712be4e5
eschnett <eschnett@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-22 22:26:50 +0000

    Add missing </ul>

--- documentation/index.php
+++ documentation/index.php
@@ -1,9 +1,17 @@
+<!-- -*-mode:html;coding:utf-8-*- -->
 <?php
 $title='Documentation';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 
-<p>documentation start page</p>
+<p>Cactus Documentation</p>
+
+<ul>
+
+  <li> <a href="UsersGuide.pdf">Users' Guide</a> </li>
+  <li> <a href="RerefenceManual.pdf">Reference Manual</a> </li>
+
+</ul>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
