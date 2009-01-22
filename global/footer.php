176266e704556267153257de0806213d08985081
eschnett <eschnett@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-22 22:53:31 +0000

    Update year

--- global/footer.php
+++ global/footer.php
@@ -19,7 +19,7 @@
 	</div> <!-- closing content-wrap div -->	
 
 	<div id="footer">
-		<p><strong>cactuscode.org</strong> 1998-2008 | Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a>/<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a></p>
+		<p><strong>cactuscode.org</strong> 1998-2009 | Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a>/<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a></p>
 	</div> <!-- closing footer div -->
 </div> <!-- wrap ends here -->
 
