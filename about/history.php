71c48c15692eac78100232f2a0465515519dc8d8
Erik Schnetter <213514+eschnett@users.noreply.github.com>
2009-01-22 21:27:05 +0000

    Update Cactus history

--- about/history.php
+++ about/history.php
@@ -3,17 +3,58 @@ $title='History';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 
-<p>Cactus is a direct descendant of many years of code development in Ed Seidel's group of researchers at the National Center for Supercomputing Applications (NCSA) as the group wrestled to numerically solve Einstein's Equations for General Relativity and thus model black holes, neutron stars and boson stars.</p>
+<p>
+Cactus is a direct descendant of many years of code development in Ed
+Seidel's group of researchers at the National Center for
+Supercomputing Applications (NCSA) as the group wrestled to
+numerically solve Einstein's Equations for General Relativity and thus
+model black holes, neutron stars, and boson stars.
+</p>
 
-<p>In 1995, Ed Seidel and many of his group moved to the Max Planck Institute for Gravitational Physics (Albert Einstein Institute or AEI) in Potsdam, Germany. Frustrated by the difficulties of coordinating the development and use of several different codes across a large collaborative group, Paul Walker, Joan Masso, Ed Seidel, and John Shalf, designed and wrote the original version of Cactus, Cactus 1.0, which provided a collaborative parallel toolkit for numerical relativity.</p>  
+<p>
+In 1995, Ed Seidel and many of his group moved to the Max Planck
+Institute for Gravitational Physics (Albert Einstein Institute or AEI)
+in Potsdam, Germany.  Frustrated by the difficulties of coordinating
+the development and use of several different codes across a large
+collaborative group, Paul Walker, Joan Masso, Ed Seidel, and John
+Shalf, designed and wrote the original version of Cactus, Cactus 1.0,
+which provided a collaborative parallel toolkit for numerical
+relativity.
+</p>
 
-<p>As Cactus was used over the next years for research in Numerical Relativity, it was realized that we could easily rewrite the code in such a way that it could be used not just by numerical relativists, but also by other computational scientists in other domains with similar needs for large scale computing. This lead to a major redesign and rewrite of Cactus, learning from all our previous experiences. In July 1999 Cactus 4.0 Beta 1 was finally released, with the main development coming from Tom Goodale, Joan Masso, Gabrielle Allen, Gerd Lanfermann and John Shalf.</p>
+<p>
+As Cactus was used over the next years for research in Numerical
+Relativity, it was realized that we could easily rewrite the code in
+such a way that it could be used not just by numerical relativists,
+but also by other computational scientists in other domains with
+similar needs for large scale computing.  This lead to a major
+redesign and rewrite of Cactus, learning from all our previous
+experiences.  In July 1999, Cactus 4.0 Beta 1 was finally released,
+with the main development coming from Tom Goodale, Joan Massó,
+Gabrielle Allen, Gerd Lanfermann and John Shalf.
+</p>
 
-<p>We are (unfortunately) still on the Cactus 4.0 release cycle, but lots has happened since 1999. Cactus has been adopted not just by application developers, but also by computer science researchers, and has been involved in many prize winning efforts for large scale computing, grid computing and high speed networks. More information about these awards can be found on our News pages.</p>
+<p>
+We are (unfortunately) still on the Cactus 4.0 release cycle, but lots
+has happened since 1999.  Cactus has been adopted not just by
+application developers, but also by computer science researchers, and
+has been involved in many prize winning efforts for large scale
+computing, grid computing and high speed networks.  More information
+about these awards can be found on our News pages.
+</p>
 
-<p>In 2003, several members of the AEI Cactus group left Germany to help found the Center for Computation & Technology (CCT) at Louisiana State University with new director Ed Seidel. Cactus is now developed and supported by a close-knit team spanning AEI and CCT.</p>
+<p>
+In 2003, several members of the AEI Cactus group left Germany to help
+found the Center for Computation &amp; Technology (CCT) at Louisiana
+State University with new director Ed Seidel.  Cactus is now developed
+and supported by a close-knit team spanning AEI and CCT.</p>
 
-<p>Along the way, many other friends and colleagues have contributed to Cactus, including Wai-Mo Suen, Thomas Dramlitsch, Thomas Schweizer, Karen Camarda, Malcolm Tobias, Mark Miller, Ed Evans, Werner Benger, Bernd BrÃ¼gmann, Ian Hawke, Andre Merzky, and Bernard Schutz.</p>
+<p>
+Along the way, many other friends and colleagues have contributed to
+Cactus, including Wai-Mo Suen, Thomas Dramlitsch, Thomas Schweizer,
+Karen Camarda, Malcolm Tobias, Mark Miller, Ed Evans, Werner Benger,
+Bernd Brügmann, Ian Hawke, Andre Merzky, and Bernard Schutz.
+</p>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
