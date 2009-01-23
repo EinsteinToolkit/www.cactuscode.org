8781004417592761ddebed2251fc1793f659c42c
eschnett <eschnett@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-23 00:12:44 +0000

    Update machine list

--- download/configfiles/index.php
+++ download/configfiles/index.php
@@ -1,6 +1,86 @@
-<?php $title='page title goes here';
+<!-- -*-mode:html;coding:utf-8-*- -->
+<?php $title='Configuration Files';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
-<p>content paragraph</p>
+<!-- 2009-01-22 Erik Schnetter: This file is currently manually --
+  -- created from information stored in the SimFactory machine --
+  -- database.  This process should be automated.  -->
+
+
+
+<h2>Supported Architectures</h2>
+
+<ul>
+  
+  <li>Linux Clusters</li>
+  <ul>
+    <li><a href="http://www.ncsa.uiuc.edu/UserInfo/Resources/Hardware/Intel64Cluster/">Abe</a>
+      (NCSA)</li>
+    <li><a href="http://supercomputers.aei.mpg.de/damiana">Damiana</a>
+    (AEI)</li>
+    <li><a href="http://www.loni.org/systems/system.php?system=Eric">Eric</a>,
+      <a href="http://www.loni.org/systems/system.php?system=Louie">Louie</a>,
+      <a href="http://www.loni.org/systems/system.php?system=Oliver">Oliver</a>,
+      <a href="http://www.loni.org/systems/system.php?system=Poseidon">Poseidon</a>,
+      <a href="http://www.loni.org/systems/system.php?system=QueenBee">Queen
+      Bee</a>, <a href="http://www.hpc.lsu.edu/systems/system.php?system=Tezpur">Tezpur</a>
+      (LONI/LSU)</li>
+    <li><a href="http://www.tacc.utexas.edu/services/userguides/lonestar/">Lonestar</a>
+    (TACC)</li>
+    <li><a href="http://www.tacc.utexas.edu/services/userguides/ranger/">Ranger</a>
+    (TACC)</li>
+  </ul>
+
+  <li>Cray XT4</li>
+  <ul>
+    <li><a href="http://www.nersc.gov/nusers/systems/franklin/">Franklin</a>
+    (NERSC)</li>
+    <li><a href="http://www.nics.tennessee.edu/computing-resources/kraken">Kraken</a>
+    (NICS)</li>
+  </ul>
+  
+  <li>IBM Blue Gene/P</li>
+  <ul>
+    <li><a href="http://www.alcf.anl.gov/resources/storage.php">Surveyor</a> (ALCF)</li>
+  </ul>
+  
+  <li>IBM P5</li>
+  <ul>
+    <li><a href="http://www.loni.org/systems/system.php?system=Bluedawg">Bluedawg</a>,
+    <a href="http://www.loni.org/systems/system.php?system=Ducky">Ducky</a>,
+    <a href="http://www.loni.org/systems/system.php?system=LaCumba">LaCumba</a>,
+    <a href="http://www.loni.org/systems/system.php?system=Neptune">Neptune</a>,
+    <a href="http://www.loni.org/systems/system.php?system=Zeke">Zeke</a>
+    (LONI)</li>
+    <li><a href="http://www.hpc.lsu.edu/systems/system.php?system=Pelican">Pelican</a>
+    (LSU)</li>
+  </ul>
+  
+  <li>SGI Altix</li>
+  <ul>
+    <li><a href="http://www.lrz-muenchen.de/services/compute/hlrb/">HLRB
+        II</a> (LRZ)</li>
+    <li><a href="http://www.hpc.lsu.edu/systems/system.php?system=Santaka"Santaka</a>
+    (LSU)</li>
+  </ul>
+  
+  <li>SiCortex</li>
+  <ul>
+    <li><a href="http://sicortex.com/">SiCortex</a> (Houston)</li>
+  </ul>
+  
+  <li>Linux Laptops/Workstations</li>
+  <ul>
+    <li><a href="http://www.cct.lsu.edu/">numrel02</a> (CCT, Linux
+    workstation)</li>
+  </ul>
+  
+  <li>Mac OS X Laptops/Workstations</li>
+  <ul>
+    <li><a href="http://www.cct.lsu.edu/">Redshift</a> (CCT, Mac OS X
+    laptop)</li>
+  </ul>
+  
+</ul>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
