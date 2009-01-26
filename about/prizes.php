7e33ee07ae1541d9f413e7b135c98d29a5ec2f35
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-26 20:32:35 +0000

    add the prizes page.

---
+++ about/prizes.php
@@ -0,0 +1,50 @@
+<!-- -*-mode:html;coding:utf-8-*- -->
+<?php
+$title='Cactus Prizes and Major Achievements';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+<p>
+During the last couple of years, Cactus and its developers have won 
+some of the high performance computing's most prestigious awards. Some 
+of them are presented on this page.
+</p>
+
+<ul>
+
+<li><a href="http://awards.computer.org/ana/award/viewPastRecipients.action?id=16">IEEE Sidney Fernbach Award (2006)</a>
+<br />
+Dr. Edward Seidel has been awarded one of the most prestigious awards for "outstanding contributions in the application of high performance computers using innovative approaches". The IEEE computer society recognized Seidel for outstanding contributions to the development of software for high-performance computing and grid computing to enable the collaborative numerical investigation of complex problems in physics, particularly focusing on modeling black hole collisions.
+</li>
+<br />
+
+<li><a href="http://www.sc-conference.org/sc2002/infra_band.html">High-Performance Bandwidth Challenge (SC2002)</a>
+<br />
+The LBNL/NCSA team has won the "Highest Performing Application" award at SuperComputing 2002 with the Cactus based application “Wide Area Distributed Simulations using Cactus, Globus and Visapult”. The wininig application transfers data at 16 GByte/s, modeling gravitational waves generated during the collision of black holes.
+</li>
+<br />
+
+<li><a href="http://www.sc-conference.org/sc2002/infra_band.html">High-Performance Computing Challenge Award (SC2002)</a>
+<br />
+The Cactus team were a core part of the Global Grid Testbed Collaboration, who won both the "Most Geographically Distributed Application" and "Most Heterogeneous Set of Platforms" prizes at SC2002 for a task farming application written with Cactus which deployed Cactus Black Hole simulations across 70 diverse machines in 12 different countries.
+</li>
+<br />
+
+<li><a href="http://www.sc2001.org/PR-20011115.shtml">Gordon Bell Prize for Supercomputing (SC2001)</a>
+<br />
+The Cactus team were a core part of the Global Grid Testbed Collaboration, who won both the "Most Geographically Distributed Application" and "Most Heterogeneous Set of Platforms" prizes at SC2002 for a task farming application written with Cactus which deployed Cactus Black Hole simulations across 70 diverse machines in 12 different countries.
+</li>
+<br />
+
+<li><a href="http://jean-luc.aei.mpg.de/Projects/SC98/">HPC “Most Stellar” Challenge Award (SC98)</a>
+<br />
+"Cactus 3" won the Most Stellar HPC Challenge Award at SC98. This application was used to perform a simulation of colliding neutron stars across the two continents, distributing the computational grid across three T3E's in Garching (Germany), Berlin (Germany) and SDSC (USA).
+</li>                                                                                                       <br />
+
+
+</ul>
+
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
+?>
