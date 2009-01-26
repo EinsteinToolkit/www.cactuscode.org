ff2044c3b31fdcaba14545093f66e5d9e5016c7c
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-26 17:20:52 +0000

    add team members...
    Jian

---
+++ about/team/ebentivegna.php
@@ -0,0 +1,20 @@
+<?php $title='Gabrielle Allen';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
+
+<p><img class="float-right" width="150" src="gallen.jpg" alt="Photo of Gabrielle Allen"/></p>
+
+<h3>Position</h3>
+<ul>
+<li>Postdoctoral researcher, Louisiana State University</li>
+</ul>
+
+<h3>Background</h3>
+<ul>
+<li>Laurea Universita' di Catania, Italy </li>
+<li>PhD Numerical Relativity, Penn State University</li>
+</ul>
+
+<h3>Research</h3>
+<p></p>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
