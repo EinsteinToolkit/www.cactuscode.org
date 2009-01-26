82faee7adaf489ff08ee2af76d6c2636274dbcde
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-26 18:31:01 +0000

    added yaakoub

---
+++ about/team/yelkhamra.php
@@ -0,0 +1,23 @@
+<?php $title='Yaakoub El-Khamra';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
+
+<p><img class="float-right" width="150" src="yelkhamra.jpg" alt="Photo of Yaakoub El-Khamra"/></p>
+
+<h3>Position</h3>
+<ul>
+<li>Research Engineering / Scientist Associate III at Texas Advanced Computing Center</li>
+</ul>
+
+<h3>Background</h3>
+<ul>
+<li>BE Mechanical Engineering American University of Beirut, Lebanon</li>
+<li>MS Petroleum Engineering, LSU</li>
+</ul>
+
+<h3>Research</h3>
+<p>Yaakoub's research interests lie in the areas of petroleum engineering, 
+computational fluid dynamics, computational frameworks, numerical methods 
+and high performance computing.
+</p>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
