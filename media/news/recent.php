bb43f1caa78f3337ecca0252d71616140166686b
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-26 20:43:35 +0000

    starting to move the old news stories over

--- media/news/recent.php
+++ media/news/recent.php
@@ -1,14 +1,12 @@
 <?php
 
 //these should be the most recent stories
-//the links will appear 
-
 $news_links='
-	<li><a href="/news">Cactus runs on 131,072 cores on Blue Gene/P at ANL</a></li>
-	<li><a href="/news">CCT researcher accelerates Einstein code with GPU computing</a></li>
-	<li><a href="/news">First XiRel Workshop</a></li>
-	<li><a href="/news">Cactus Benchmarks Run on Ranger</a></li>
-	<li><a href="/news">Cactus Run on First Loongson 2F Chinese Cluster</a></li>
+	<li><a href="/media/news/bgpanl">Cactus runs on 131,072 cores on Blue Gene/P at ANL</a></li>
+	<li><a href="/media/news/GPUcomputing">CCT researcher accelerates Einstein code with GPU computing</a></li>
+	<li><a href="/media/news/XiRelWorkshop08">First XiRel Workshop</a></li>
+	<li><a href="/media/news/Ranger">Cactus Benchmarks Run on Ranger</a></li>
+	<li><a href="/media/news/CactusLoongson">Cactus Run on First Loongson 2F Chinese Cluster</a></li>
 ';
 
 $news_blurbs='
