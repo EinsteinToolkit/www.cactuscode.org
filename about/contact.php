908da026c62ebd202b1060b70db04f5486c84452
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-26 18:47:08 +0000

    update the contact pages
    Jian

--- about/contact.php
+++ about/contact.php
@@ -3,6 +3,10 @@
 $title='Contact';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
+<p>We recommend you to use our <a href="http://www.cactuscode.org/mailman/listinfo/">Mailing Lists</a> to ask questions about Cactus and how to use and develop for it. But if you want to contact the Cactus Team directly, you can mail to <a href="mailto:cactusmaint@cactuscode.org">cactusmaint@cactuscode.org</a>. 
+</p><br />
+We do welcome hearing about what you are using Cactus for, what your experiences are, and how to improve it. 
+
 
 <p>Internal Support
 <br />
