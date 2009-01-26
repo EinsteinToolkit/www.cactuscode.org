6e5a7664ce289d1fa386d9923ab7080bad486836
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-26 18:51:16 +0000

    removed the internal support line.

--- about/contact.php
+++ about/contact.php
@@ -4,14 +4,8 @@ $title='Contact';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 <p>We recommend you to use our <a href="http://www.cactuscode.org/mailman/listinfo/">Mailing Lists</a> to ask questions about Cactus and how to use and develop for it. But if you want to contact the Cactus Team directly, you can mail to <a href="mailto:cactusmaint@cactuscode.org">cactusmaint@cactuscode.org</a>. 
-</p>
-<br />
-<p>We do welcome hearing about what you are using Cactus for, what your experiences are, and how to improve it.</p>
-<br />
-<p>Internal Support
-<br />
-Colby Jordan | cjordan@cct.lsu.edu</p>
-
+We do welcome hearing about what you are using Cactus for, what your experiences are, and how to improve it.</p>
+<p>Please contact our webmaster Colby Jordan | cjordan@cct.lsu.edu if you have any questions or suggestions with regard to the Cactus website.</p>
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
 ?>
