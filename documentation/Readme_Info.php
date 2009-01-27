5e24c456ebd849c4baa0bc478a88f82a47716d10
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-27 20:37:38 +0000

    test

--- documentation/Readme_Info.php
+++ documentation/Readme_Info.php
@@ -2,6 +2,11 @@
 <?php $title='Overview of Thorn README files';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
+<p>
+Note: The maintainer of all thorns is the Cactus team and the licence of
+all thorns is LGPL.
+</p>
+
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/documentation/Readme_Info.inc');?>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
