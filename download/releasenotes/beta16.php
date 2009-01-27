4b498e93e9459e0a74ed758be1577cfdf772e6a0
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-01-27 21:28:03 +0000

    relnotes formatting

--- download/releasenotes/beta16.php
+++ download/releasenotes/beta16.php
@@ -1,6 +1,6 @@
 <?php $title='Release Notes for B16 from CVS';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
-<p><?php include_once('/var/www/cvs/Utilities/Releases/Beta16/ReleaseNotesBeta16.txt');?></p>
+<p><pre><?php include_once('/var/www/cvs/Utilities/Releases/Beta16/ReleaseNotesBeta16.txt');?></pre></p>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
