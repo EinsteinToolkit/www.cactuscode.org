0a3c22a1efc6f6dcbf8a1a5b358372f18e718494
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-02-05 20:18:53 +0000

    wrote a script for making new news stories; adding story for latest beta release

--- media/news/recent.php
+++ media/news/recent.php
@@ -2,6 +2,7 @@
 
 //these should be the most recent stories
 $news_links='
+	<li><a href="/media/news/c4.0beta16">Cactus 4.0 Beta 16 release</a></li>
 	<li><a href="/media/news/bgpanl">Cactus runs on 131,072 cores on Blue Gene/P at ANL</a></li>
 	<li><a href="/media/news/GPUcomputing">CCT researcher accelerates Einstein code with GPU computing</a></li>
 	<li><a href="/media/news/XiRelWorkshop08">First XiRel Workshop</a></li>
