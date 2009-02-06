cfa75feb6b8a9e916afa7625d1bbe7497ae93b70
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-02-06 03:06:28 +0000

    much better script for adding news stories in a more automated way

--- media/news/index.php
+++ media/news/index.php
@@ -1,9 +1,6 @@
 <?php $title='News';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 
-//include if it hasn't already been on the side
-include_once($_SERVER['DOCUMENT_ROOT'].'/media/news/recent.php');
-
 //don't edit this file
 //edit the file at /media/news/recent.php to change the current news stories
 
