8a86496cddcf9b8df7099fda9afd672efa0bdd29
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-04-15 19:39:14 +0000

    some posters

--- media/index.php
+++ media/index.php
@@ -3,7 +3,7 @@ $title='Media';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 
-<p>news, presentations, publications, prizes</p>
+<p>Use the links on the right to check out the media subections, news, presentations, publications, prizes, where you can find out more about the history and accomplishments of the Cactus Code project.</p>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
