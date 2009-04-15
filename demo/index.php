509672e7bc615dc0d6da32ca6e38e53725eea9d6
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-04-15 19:39:14 +0000

    some posters

---
+++ demo/index.php
@@ -0,0 +1,8 @@
+<?php $title='Presentations';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
+
+<p>landing page with explanation of demo</p>
+
+<!-- screenshot -->
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
--- media/presentations/index.php
+++ media/presentations/index.php
@@ -1,6 +1,37 @@
 <?php $title='Presentations';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
-<p>content paragraph</p>
+<h1>Posters and Videos</h1>
+<table>
+<tr>
+<td><a href="APS_08_Poster.pdf"><img src="APS_08_Poster.jpg" width="120"></a></td>
+<td><i>Binary Neutron Star Evolution with Adaptive Mesh Refinement Methods.</i> J. Tao, W.-M. Suen, R. Wolfmeyer and H.-M. Zhang,  American Physical Society Meeting, April 2008, St. Louis, Mo., USA.</td>
+</tr>
+
+
+<tr>
+<td><a href="AlpacaPoster.pdf"><img src="Alpaca.jpg" width="120"></a></td>
+<td><i> Alpaca: Cactus Tools for Application Level Proﬁling and Correctness Analysis.</i> E. Schnetter, G. Allen, T. Goodale and M. Tyagi, Building Petascale Applications and Software Environments on the Teragrid Workshop, December 11-12, 2007, Arizona State University, Tempe, Az., USA.</td>
+</tr>
+
+
+<tr>
+<td><a href="XiRel_Poster.pdf"><img src="XiRel_Poster.jpg" width="120"></a></td>
+<td><i>Next Generation Infrastructure for Numerical Relativity.</i> G. Allen, M. Campanelli, P. Laguna, C. Lousto, D. Shoemaker, E. Schnetter and E. Seidel, CBuilding Petascale Applications and Software Environments on the Teragrid Workshop, December 11-12, 2007, Arizona State University, Tempe, Az., USA.</td>
+</tr>
+
+
+<tr>
+<td><a href="DrillingLabPoster.pdf"><img src="DrillingLabPoster.jpg" width="120"></a></td>
+<td><i>Drilling Lab - A Sensor and Computation Grid Enabled Engineering Model for Drilling Vibration Research.</i> Richard Duff and Yaakoub El Khamra, Mardi Gras Conference 2008, Baton Rouge, La., USA.</td>
+</tr>
+
+<p>
+
+<tr>
+<td><a href="http://www.youtube.com/watch?v=SFUG6itL_0M"><img src="GravityVideo.jpg" width="120"></a></td>
+<td><i><a href="http://www.youtube.com/watch?v=SFUG6itL_0M">Tackling the Riddles of Gravity (Visions of Space Time)</a>.</i> Video compilation and direction: Werner Benger, 2003.</td>
+</tr>
+</table>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
