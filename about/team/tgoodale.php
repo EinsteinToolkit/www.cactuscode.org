212709c8fd7918b560ea108095879246092406b4
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-04-22 01:41:58 +0000

    placeholder pages for cactus team

---
+++ about/team/tgoodale.php
@@ -0,0 +1,19 @@
+<?php $title='Tom Goodale';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
+
+<p><img class="float-right" width="150" src="tgoodale.jpg" alt="Photo of Tom Goodale"/></p>
+
+<h3>Position</h3>
+<ul>
+<li></li>
+</ul>
+
+<h3>Background</h3>
+<ul>
+<li></li>
+</ul>
+
+<h3>Research</h3>
+<p></p>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
