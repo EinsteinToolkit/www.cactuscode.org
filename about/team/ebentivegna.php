560ac7779f016bcd3c51acdbbb2b878eeaad3491
bentivegna <bentivegna@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-04-28 16:29:23 +0000

    Added info in my profile.

--- about/team/ebentivegna.php
+++ about/team/ebentivegna.php
@@ -1,7 +1,7 @@
 <?php $title='Eloisa Bentivegna';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
-<p><img class="float-right" width="150" src="ebentivegna.jpg" alt="Photo of Eloisa Bentivegna"/></p>
+<p><img class="float-right" width="150" src="bentivegna.png" alt="Photo of Eloisa Bentivegna"/></p>
 
 <h3>Position</h3>
 <ul>
@@ -10,11 +10,16 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <h3>Background</h3>
 <ul>
-<li>Laurea Universita' di Catania, Italy </li>
-<li>PhD Numerical Relativity, Penn State University</li>
+<li>Laurea in Fisica, Universit&agrave; di Catania (Italy) </li>
+<li>PhD Physics - minor in High Performance Computing, Penn State University (Italy)</li>
 </ul>
 
 <h3>Research</h3>
-<p></p>
+<p>My research is centered around the numerical simulation of systems governed by the 
+laws of General Relativity, ranging from black holes to the large-scale universe, and in 
+the computational tools required. In particular, I develop high-level debugging and 
+visualization tools within the <a href="http://www.cactuscode.org/Development/alpaca">Alpaca</a> 
+project and have an interest in numerical methods for the extraction of physical 
+observables from the simulations (horizon analysis, wave extraction).</p>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
