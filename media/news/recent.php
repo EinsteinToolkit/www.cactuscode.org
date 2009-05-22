56bae80756ebd442813b2bf25393d9ec1f1d3bd4
eschnett <eschnett@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-05-22 17:04:44 +0000

    Rename some news items

--- media/news/recent.php
+++ media/news/recent.php
@@ -1,10 +1,23 @@
-<li><a href="/media/news/BH-to-GRB-SiCortex">[March 12, 2009]<br />Webcast: <i>From Black Holes to Gamma-Ray Bursts</i>, hosted by SiCortex</a></li>
+<li><a href="/media/news/BH-to-GRB-SiCortex">
+    [March 17, 2009]<br />
+    Webcast: <i>From Black Holes to Gamma-Ray Bursts</i>, hosted by
+    SiCortex
+</a></li>
 
-<li><a href="/media/news/c4.0beta16">Cactus 4.0 Beta 16 release</a></li>
+<li><a href="/media/news/Cactus4.0beta16-release">
+    [February 3, 2009]<br />
+    Cactus 4.0 beta 16 released
+</a></li>
 
-<li><a href="/media/news/sc08alpaca/">Two ALPACA tools displayed at SC08</a></li>
+<li><a href="/media/news/SC08-Alpaca">
+    [December 2, 2008]<br />
+    Cactus team demonstrates Alpaca tools at SuperComputing 2008
+</a></li>
 
-<li><a href="/media/news/bgpanl">Cactus runs on 131,072 cores on Blue Gene/P at ANL</a></li>
+<li><a href="/media/news/BGP-131072">
+    [August 16, 2008]<br />
+    Cactus runs on 131,072 cores on Blue Gene/P at ANL
+</a></li>
 
 <li><a href="/media/news/GPUcomputing">CCT researcher accelerates Einstein code with GPU computing</a></li>
 
