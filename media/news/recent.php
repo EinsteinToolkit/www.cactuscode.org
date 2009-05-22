ed3052ef5b7680e567313519fed69b4206344cc3
eschnett <eschnett@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-05-22 16:56:21 +0000

    Rename SiCortex webcast news item

--- media/news/recent.php
+++ media/news/recent.php
@@ -1,8 +1,15 @@
-	<li><a href="/media/news/scicortexwebcast">Webcast on Using Cactus for Gravitational Physics Simulations</a></li>
-	<li><a href="/media/news/c4.0beta16">Cactus 4.0 Beta 16 release</a></li>
-	<li><a href="/media/news/sc08alpaca/">Two ALPACA tools displayed at SC08</a></li>
-	<li><a href="/media/news/bgpanl">Cactus runs on 131,072 cores on Blue Gene/P at ANL</a></li>
-	<li><a href="/media/news/GPUcomputing">CCT researcher accelerates Einstein code with GPU computing</a></li>
-	<li><a href="/media/news/XiRelWorkshop08">First XiRel Workshop</a></li>
-	<li><a href="/media/news/Ranger">Cactus Benchmarks Run on Ranger</a></li>
-	<li><a href="/media/news/CactusLoongson">Cactus Run on First Loongson 2F Chinese Cluster</a></li>
+<li><a href="/media/news/BH-to-GRB-SiCortex">[March 12, 2009]<br />Webcast: <i>From Black Holes to Gamma-Ray Bursts</i>, hosted by SiCortex</a></li>
+
+<li><a href="/media/news/c4.0beta16">Cactus 4.0 Beta 16 release</a></li>
+
+<li><a href="/media/news/sc08alpaca/">Two ALPACA tools displayed at SC08</a></li>
+
+<li><a href="/media/news/bgpanl">Cactus runs on 131,072 cores on Blue Gene/P at ANL</a></li>
+
+<li><a href="/media/news/GPUcomputing">CCT researcher accelerates Einstein code with GPU computing</a></li>
+
+<li><a href="/media/news/XiRelWorkshop08">First XiRel Workshop</a></li>
+
+<li><a href="/media/news/Ranger">Cactus Benchmarks Run on Ranger</a></li>
+
+<li><a href="/media/news/CactusLoongson">Cactus Run on First Loongson 2F Chinese Cluster</a></li>
