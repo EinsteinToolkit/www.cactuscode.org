c616a3c5b16c4191d6c98bac31573543022ed24d
Erik Schnetter <213514+eschnett@users.noreply.github.com>
2009-05-25 17:05:55 +0000

    Update SiCortex news item

--- media/news/BH-to-GRB-SiCortex/index.php
+++ media/news/BH-to-GRB-SiCortex/index.php
@@ -2,9 +2,40 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 
+<h3>
+May 25, 2009:<br />
+<i>From Black Holes to Gamma-Ray Bursts</i> Webcast Available
+</h3>
+
 <p>
-Write the story here.
+LSU Center for Computation &amp; Technology Researchers Steven Brandt
+and Erik Schnetter presented the results of their research on the
+computational challenges of gravitational physics, the use of the
+Cactus framework for modern black hole evolutions and modeling
+Gamma-Ray Bursts (GRB) in a recent webcast, hosted by SiCortex, Inc.
 </p>
 
-<h3 class="align-right">13 May 2009 — cjordan</h3>
+<p>
+LSU has developed a Cactus simulation of an inspiraling binary black
+hole system using over 5,800 cores for large-scale testing and online
+remote visualization.  At SuperComputing 2008, the largest uniform
+grid evolution of a black hole collision to-date was performed using
+an 800<sup>3</sup> sized data set.
+</p>
+
+<p>
+During the webcast, Drs. Schnetter and Brandt also discuss how their
+simulation results will be used to carry out further scientific
+research and identify ways to improve scaling and performance.
+</p>
+
+<p>
+This webcast is part of a series of Science-to-Science webcasts
+sponsored by SiCortex and intended to highlight the use of HPC systems
+in advancing scientific discovery.  The webcast and associated white
+paper, Relativistic Astrophysics on the SiCortex Architecture, can be
+viewed <a href="http://sicortex.com/news_events/campaigns/lsu_webinar/cactus_resources">here</a>.
+</p>
+
+<h3 class="align-right">25 May 2009 — Erik Schnetter</h3>
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
