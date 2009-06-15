a8a1ca1bdcafd1cc8e37d3928670bfe991705eac
jtao <jtao@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-06-15 21:54:54 +0000

    ordered by year. Jian

--- media/presentations/index.php
+++ media/presentations/index.php
@@ -2,28 +2,39 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <h1>Posters and Videos</h1>
+
 <table>
+
+<tr>
+<td>2009</td> <td></td>
+</tr>
+
 <tr>
 <td><a href="APS_08_Poster.pdf"><img src="APS_08_Poster.jpg" width="120"></a></td>
 <td><i>Binary Neutron Star Evolution with Adaptive Mesh Refinement Methods.</i> J. Tao, W.-M. Suen, R. Wolfmeyer and H.-M. Zhang,  American Physical Society Meeting, April 2008, St. Louis, Mo., USA.</td>
 </tr>
 
+<tr>
+<td>2008</td> <td></td>
+</tr>
+
+<tr>
+<td><a href="DrillingLabPoster.pdf"><img src="DrillingLabPoster.jpg" width="120"></a></td>
+<td><i>Drilling Lab - A Sensor and Computation Grid Enabled Engineering Model for Drilling Vibration Research.</i> Richard Duff and Yaakoub El Khamra, Mardi Gras Conference 2008, Baton Rouge, La., USA.</td>
+</tr>
+
+<tr>
+<td>2007</td> <td></td>
+</tr>
 
 <tr>
 <td><a href="AlpacaPoster.pdf"><img src="Alpaca.jpg" width="120"></a></td>
 <td><i> Alpaca: Cactus Tools for Application Level Proﬁling and Correctness Analysis.</i> E. Schnetter, G. Allen, T. Goodale and M. Tyagi, Building Petascale Applications and Software Environments on the Teragrid Workshop, December 11-12, 2007, Arizona State University, Tempe, Az., USA.</td>
 </tr>
 
-
 <tr>
 <td><a href="XiRel_Poster.pdf"><img src="XiRel_Poster.jpg" width="120"></a></td>
 <td><i>Next Generation Infrastructure for Numerical Relativity.</i> G. Allen, M. Campanelli, P. Laguna, C. Lousto, D. Shoemaker, E. Schnetter and E. Seidel, CBuilding Petascale Applications and Software Environments on the Teragrid Workshop, December 11-12, 2007, Arizona State University, Tempe, Az., USA.</td>
-</tr>
-
-
-<tr>
-<td><a href="DrillingLabPoster.pdf"><img src="DrillingLabPoster.jpg" width="120"></a></td>
-<td><i>Drilling Lab - A Sensor and Computation Grid Enabled Engineering Model for Drilling Vibration Research.</i> Richard Duff and Yaakoub El Khamra, Mardi Gras Conference 2008, Baton Rouge, La., USA.</td>
 </tr>
 
 <p>
