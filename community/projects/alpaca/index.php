0facd1fb5fa4634948b2df39d10054d2029a889f
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-06-30 21:25:32 +0000

    alpaca set up

--- community/projects/alpaca/index.php
+++
@@ -1,6 +0,0 @@
-<?php $title='page title goes here';
-include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
-
-<p>content paragraph</p>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
