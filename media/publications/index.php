379653d9b124fd2f91f3d9abe54de38862af6087
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-07-13 17:00:46 +0000

    many small changes

--- media/publications/index.php
+++ media/publications/index.php
@@ -3,9 +3,7 @@ $title='Publications';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 
-<h1>Cactus Publications</h1>
-
-<p>We have them in <a href="cactus-publications.html">html</a> and as
+<p>Cactus publications are available in <a href="cactus-publications.html">html</a> and as
   <a href="cactus-publications.pdf">PDF</a>, and in
   raw <a href="cactus-publications.bib">bibtex</a>.</p>
 
