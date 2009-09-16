9f71cb7e0126920fe27e270857bc354461009d70
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-09-16 19:37:10 +0000

    couple files left out

---
+++ media/publications/example.php
@@ -0,0 +1,10 @@
+<!-- -*-mode:html;coding:utf-8-*- -->
+<?php $title='Publications';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
+
+<center><h3>Also available in <a href="">PDF</a>.</h3></center>
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/media/publications/cactus-publications.html');
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
