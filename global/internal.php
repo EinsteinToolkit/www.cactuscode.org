d9aafbe80a0fb8054b67e5d02a69779f62f5fb66
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-10-21 17:09:28 +0000

    hard-coded tutorials listing and some stylesheet changes

--- global/internal.php
+++ global/internal.php
@@ -49,21 +49,21 @@ $LOGIN_INFORMATION = array(
 // Add login/password pairs below, like described above
 // NOTE: all rows except last must have comma "," at the end of line
 $LOGIN_INFORMATION = array(
-  'cactus' => 'code',
+  'user' => 'cactuscode',
 );
 
 // request login? true - show login and password boxes, false - password box only
-define('USE_USERNAME', true);
+define('USE_USERNAME', false);
 
 // User will be redirected to this page after logout
 define('LOGOUT_URL', 'http://cactuscode.org/');
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
-define('TIMEOUT_MINUTES', 0);
+define('TIMEOUT_MINUTES', 120);
 
 // This parameter is only useful when TIMEOUT_MINUTES is not zero
 // true - timeout time from last activity, false - timeout time from login
-define('TIMEOUT_CHECK_ACTIVITY', false);
+define('TIMEOUT_CHECK_ACTIVITY', true);
 
 ##################################################################
 #  SETTINGS END
