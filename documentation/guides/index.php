784fb972a922035223e9214945028aec68974adf
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-10-27 04:35:06 +0000

    moving guides

--- documentation/guides/index.php
+++
@@ -1,6 +0,0 @@
-<?php $title='Guides';
-include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
-
-<p><?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/tools/ls.php'); ?></p>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
