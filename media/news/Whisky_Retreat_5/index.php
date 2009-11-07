82e858632bbc1ce89db4a1d74ca06e8a8c333ee1
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-11-07 20:14:16 +0000

    news item for Whisky retreat, analog to item for second retreat

---
+++ media/news/Whisky_Retreat_5/index.php
@@ -0,0 +1,14 @@
+<?php $title='Whisky Retreat V';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+<p>
+The fifth Whisky Retreat, the annual meeting of the developers of the
+<a href="http://www.whiskycode.org/">Whisky code</a> will be held from
+December 17-18th 2009 at the University of Southampton in the UK. More
+information can be found
+<a href=http://www.whiskycode.org/events/whiskyretreatV/WhiskyRetreat_V.html">here</a>.
+</p>
+
+<h3 class="align-right">7 November 2009 — knarf</h3>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
