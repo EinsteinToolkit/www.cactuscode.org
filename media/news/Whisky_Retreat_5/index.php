a0b6c4639f3b32fcde68807c9122a4557073be0a
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-11-07 20:28:34 +0000

    html fix

--- media/news/Whisky_Retreat_5/index.php
+++ media/news/Whisky_Retreat_5/index.php
@@ -7,7 +7,7 @@ The fifth Whisky Retreat, the annual meeting of the developers of the
 <a href="http://www.whiskycode.org/">Whisky code</a> will be held from
 December 17-18th 2009 at the University of Southampton in the UK. More
 information can be found
-<a href=http://www.whiskycode.org/events/whiskyretreatV/WhiskyRetreat_V.html">here</a>.
+<a href="http://www.whiskycode.org/events/whiskyretreatV/WhiskyRetreat_V.html">here</a>.
 </p>
 
 <h3 class="align-right">7 November 2009 — knarf</h3>
