f42bb8d593909e2718aa3d7a426e4f30e4de7db6
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2009-11-30 18:54:45 +0000

    publications landing page update

--- media/publications/index.php
+++ media/publications/index.php
@@ -3,9 +3,13 @@ $title='Publications';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 $section='media';?>
 
-<p>Cactus publications are available in <a href="cactus-publications.html">html</a> and as
+<p>A listing of publications and websites involving Cactus is available in <a href="cactus-publications.html">html</a>, as
   <a href="cactus-publications.pdf">PDF</a>, and in
   raw <a href="cactus-publications.bib">bibtex</a>.</p>
+
+<p>
+Visualizations created with Cactus have been featured on the <a href="covers">cover of many major publications</a>.
+</p>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
