30be2d6473603990f128216904bce97942a441be
Erik Schnetter <213514+eschnett@users.noreply.github.com>
2010-01-25 19:25:47 +0000

    Complete "Cactus projects" page

--- community/projects/index.php
+++ community/projects/index.php
@@ -2,10 +2,20 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <p>
-<ul>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/tools/ls.php'); ?>
-</ul>
+  The Cactus framework is the basis for a set of other projects that
+  build on the infrastructure that it provides.
 </p>
+
+<ul>
+  <li>
+    <a href="alpaca">Alpaca</a> develops Cactus Tools for Application
+    Level Profiling and Correctness Analysis
+  </li>
+  <li>
+    <a href="http://www.cct.lsu.edu/xirel">XiRel</a> develops a Next
+    Generation Infrastructure for Numerical Relativity
+  </li>
+</ul>
 
 <br /><br /><br />
 
