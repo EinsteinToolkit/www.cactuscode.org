efd6861109258ee664f3723b96dec24be38a88c5
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-04-20 18:56:39 +0000

    cvs -> svn

--- download/index.php
+++ download/index.php
@@ -18,65 +18,61 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <p>
   Cactus is maintained
-  using <a href="http://www.nongnu.org/cvs/">CVS</a> (Concurrent
-  Versions System).  The preferred method is to use
-  the <kbd>GetCactus</kbd> script along with a thornlist as described
+  using <a href="http://subversion.apache.org/">Subversion</a>.
+  The preferred method is to use
+  the <kbd>GetComponents</kbd> script along with a thornlist as described
   below.  We maintain a <a href="/download/thorns/">page of basic
   thornlists</a>, or you can use your own customized thornlist.
 </p>
 
 <p>
-  Save the <a href="GetCactus">checkout script</a> with the
-  name <kbd>GetCactus</kbd>.
+  Save the <a href="GetComponents">checkout script</a> with the
+  name <kbd>GetComponents</kbd> and make it executable.
 </p>
 
+<code>wget http://www.cactuscode.org/download/GetComponents</code>
+<code>chmod a+x GetComponents</code>
+
 <p>
-  To checkout the flesh only (compiling the flesh alone will not allow
-  you to run any applications), type the below and follow the
-  instructions:
-<code>chmod a+x GetCactus</code>
-<code>./GetCactus</code>
   To checkout an application version of Cactus, choose and save
   a <a href="thorns">ThornList</a>, or provide one to
   your own needs, and issue the command:
-<code>./GetCactus &lt;<var>thornlist-name</var>&gt;</code>
+<code>./GetComponents &lt;<var>thornlist-name</var>&gt;</code>
   To add more Thorns to your Cactus, either use
-  the <kbd>GetCactus</kbd> script with a new Thorn List, or use the
+  the <kbd>GetComponents</kbd> script with a new Thorn List, or use the
   below make target from within the Cactus distribution.
-  (Using <kbd>GetCactus</kbd> is preferred).
+  (Using <kbd>GetComponents</kbd> is preferred).
 <code>gmake checkout</code>
 </p>
 
-<h3>CVS Repositories</h3>
+<h3>Subversion Repositories</h3>
 
 <p>
-  Instead of using the <kbd>GetCactus</kbd> script, you can also check
-  out Cactus using <kbd>cvs</kbd> commands.
-  The main Cactus CVS Server (<kbd>cvs.cactuscode.org</kbd>) hosts
+  Instead of using the <kbd>GetComponents</kbd> script, you can also check
+  out Cactus using <kbd>svn</kbd> commands or another Subversion client.
+  The main Cactus Subversion Server (<kbd>svn.cactuscode.org</kbd>) hosts
   several different repositories
-  (explained <a href="/documentation/tutorials/CactusRepositories-HOWTO.txt">CactusRepositories-HOWTO</a>),
-  including different repositories for the stable and development
+<!--  (explained <a href="/documentation/tutorials/CactusRepositories-HOWTO.txt">CactusRepositories-HOWTO</a>),-->
+  including different branches for the stable and development
   versions of Cactus.
 </p>
-
+<!---
 <p>
   To checkout flesh and Thorns for the stable release, move to the
   directory where the Cactus installation should be made.  (For the
   latest development version of Cactus, use <kbd>/cactusdevcvs</kbd>
   instead of <kbd>/cactus</kbd> everywhere below).
 </p>
+-->
 
 <p>
-  Login to the CVS server, the <kbd>&lt;<var>user name</var>&gt;</kbd>
-  is <kbd>cvs_anon</kbd> and the password is <kbd>anon</kbd> (unless
-  you have a CVS account on our server in which case you can
-  alternatively use your own username and password).
-<code>cvs -d :pserver:&lt;<var>user name</var>&gt;@cvs.cactuscode.org:/cactus login</code>
+  The Subversion server can be use anonymously to checkout Cactus components.
+  It supports both http and https connections, but only https can be used for
+  authenticated logins, e.g. for commits to the repositories.
 </p>
-
 <p>
   Check out the Cactus flesh:
-<code>cvs -d :pserver:&lt;<var>user name</var>&gt;@cvs.cactuscode.org:/cactus checkout Cactus</code>
+<code>svn co http://svn.cactuscode.org/flesh/trunk Cactus</code>
   Move to the arrangements directory of the Cactus installation and
   install the required arrangements and thorns:
 <code>cd Cactus/arrangements</code>
@@ -87,13 +83,10 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 </p>
 
 <p>
-  To checkout complete arrangements (e.g. see our standard
-  <a href="/documentation/arrangements/">ThornLists</a>):
-<code>cvs -d :pserver:&lt;<var>user name</var>&gt;@cvs.cactuscode.org:/cactus checkout &lt;<var>arrangement</var>&gt;</code>
-  To checkout individual thorns (e.g. see our
-  standard <a href="/documentation/thorns/">ThornLists</a>):
-  
-<code>cvs -d :pserver:&lt;<var>user name</var>&gt;@cvs.cactuscode.org:/cactus checkout &lt;<var>arrangement</var>&gt;/&lt;<var>thorn</var>&gt;</code>
+  To checkout individual thorns (e.g. see our standard
+  <a href="/documentation/thorns/">ThornLists</a>):
+
+<code>svn co http://svn.cactuscode.org/arrangements/&lt;<var>arrangement</var>&gt;/&lt;<var>thorn</var>&gt;/trunk &lt;<var>thorn</var>&gt;</code>
 </p>
  
 <a name="requirements"/></a>
