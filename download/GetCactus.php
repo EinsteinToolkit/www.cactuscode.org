c988c7684675789d3c969136bc0527e17fd487d4
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-05-05 19:30:36 +0000

    also use GetCactus from svn

--- download/GetCactus.php
+++ download/GetCactus.php
@@ -1,4 +1,6 @@
 <?php
 header('Content-type: text/plain');
-include_once('/var/www/cvs/Utilities/Scripts/GetCactus');
+#include_once('/var/www/cvs/Utilities/Scripts/GetCactus');
+
+$gcscript = file_get_contents('https://svn.cactuscode.org/Utilities/trunk/Scripts/GetCactus');
 ?>
