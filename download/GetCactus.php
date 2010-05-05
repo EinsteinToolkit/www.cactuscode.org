599df02a0d58d3344f610dff2a658b745b4d5bed
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-05-05 19:33:09 +0000

    fix script

--- download/GetCactus.php
+++ download/GetCactus.php
@@ -3,4 +3,5 @@ header('Content-type: text/plain');
 #include_once('/var/www/cvs/Utilities/Scripts/GetCactus');
 
 $gcscript = file_get_contents('https://svn.cactuscode.org/Utilities/trunk/Scripts/GetCactus');
+echo $gcscript;
 ?>
