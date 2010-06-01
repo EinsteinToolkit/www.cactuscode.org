bf468a1b850f62e34a6308821120bc1305a29e7a
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-06-01 20:44:51 +0000

    add tutorials page

---
+++ media/tutorials/index.php
@@ -0,0 +1,17 @@
+<?php $title='Tutorials';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+$section='media';?>
+
+<h1>Cactus tutorials</h1>
+
+<table>
+ <tr><th>Date</th><th>Title</th><th>Location</th><th>Presenter</th></tr>
+ <tr><td>Apr 21 2010</td><td>Introduction to the Cactus Framework</td><td>Loni/HPC tutorial series, CCT (LSU)</td><td>Frank Löffler, Tyler Landis</td></tr>
+ <tr><td>Oct 19 2009</td><td>Introduction to the Cactus Framework</td><td>Loni/HPC tutorial series, CCT (LSU)</td><td>Frank Löffler, Tyler Landis</td></tr>
+ <tr><td>Jun 22 2009</td><td>Introduction to the Cactus Framework</td><td>TeraGrid09 conference, half-day tutorial</td><td>Frank Löffler, Oleg Korobkin</td></tr>
+ <tr><td>Jun 22 2009</td><td>Introduction to the Cactus Framework</td><td>TeraGrid09 conference, student tutorial</td><td>Erik Schnetter</td></tr>
+ <tr><td>May 27 2009</td><td>Introduction to the Cactus Framework</td><td>Loni/HPC tutorial series, CCT (LSU)</td><td>Frank Löffler, Oleg Korobkin</td></tr>
+</table>
+
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
