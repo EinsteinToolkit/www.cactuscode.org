73222c2e6886d9e431fce9006b4dd47608636aff
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-06-14 03:13:15 +0000

    test

--- demo/x.php
+++ demo/x.php
@@ -37,7 +37,7 @@ else
 <td>
 <p>
 The live site is currently at version <strong>'
-.exec('ls /var/www/ | tr -d \'\n\'')
+.exec('ls /var/www/ | tr \'\n\' \' \'')
 .'</strong>. 
 <br /><br />
 <input class="button" type="button" value="Make Recent Changes Live" onclick="window.location.href=\'/x?golive\'" />
