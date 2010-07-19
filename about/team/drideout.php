b07c95eb39aaa4ee5cee6d26c49eea93b4dc5015
rideout <rideout@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-07-19 17:21:29 +0000

    Filled out some of my page.

--- about/team/drideout.php
+++ about/team/drideout.php
@@ -5,15 +5,23 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <h3>Position</h3>
 <ul>
-<li></li>
+<li>Postdoctoral Researcher, Perimeter Institute for Theoretical Physics</li>
 </ul>
 
 <h3>Background</h3>
 <ul>
-<li></li>
+<li>Imperial College London</li>
+<li>Hamilton College</li>
+<li>Max Planck Institute for Gravitational Physics (Albert Einstein Institute)</li>
 </ul>
 
 <h3>Research</h3>
+<ul>
+<li>Causal Set Quantum Gravity</li>
+<li>Loop Quantum Gravity</li>
+<li>Cactus</li>
+<li>Numerical Relativity</li>
+</ul>
 <p></p>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
