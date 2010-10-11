459b0aedb59a5d3de3482f84623042ffd255282b
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-10-11 14:24:16 +0000

    fix link

--- community/index.php
+++ community/index.php
@@ -3,7 +3,7 @@ $title='Community';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 ?>
 
-<p>Use the subsection links at the right for information on <a href="/community/benchmarks">benchmarks</a>, the <a href="/community/bugs">bug reporting</a> system, Cactus <a href="/community/mailinglists">mailing lists</a>, Cactus partners, projects, and regression tests. </p>
+<p>Use the subsection links at the right for information on <a href="/community/benchmarks">benchmarks</a>, the <a href="https://trac.einsteintoolkit.org/">bug reporting</a> system, Cactus <a href="/community/mailinglists">mailing lists</a>, Cactus partners, projects, and regression tests. </p>
 
 <br /><br /><br />
 
