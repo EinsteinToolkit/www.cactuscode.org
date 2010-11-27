5060b65de823fa7e825ca51f0fa7c0a1abdc61fa
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2010-11-27 04:25:30 +0000

    tweak

--- global/header.php
+++ global/header.php
@@ -47,7 +47,7 @@
    <p>Warning: We currently have trouble with the cactuscode.org domain. Please use the
       following temporary
       hostnames until this is resolved. Note that ssl mismatches are to be expected, and email
-      lists will probably not get restored temporarily. Follow this page for updates.
+      lists will probably not get restored temporarily. Follow this page for updates.<br/>
       www: <a href="http://cactuscode.dyndns.org/">http://cactuscode.dyndns.org/</a>,
       svn: <a href="http://svn-cactuscode.dyndns.org/">http://svn-cactuscode.dyndns.org/</a></p>
   </div>
