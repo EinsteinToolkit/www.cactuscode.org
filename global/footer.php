9f407370b5548b6bbe1617e99e4912dbf980e416
cjordan <cjordan@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-02-01 10:58:14 +0000

    updating year range in footer to a dynamic one

--- global/footer.php
+++ global/footer.php
@@ -20,8 +20,11 @@
 	</div> <!-- closing content-wrap div -->	
 
 	<div id="footer">
-		<p><strong>cactuscode.org</strong> 1998-2009 | Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a>/<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a></p>
+		<p>
+			<strong>cactuscode.org</strong> 1998&endash;<?php echo date("Y"); ?> | Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a>/<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+		</p>
 	</div> <!-- closing footer div -->
+
 </div> <!-- wrap ends here -->
 
 <!-- Original template design provided generously by http://styleshout.com/ under a creative commons attritbution license-->
