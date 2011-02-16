e2ba286a56a5b445c0a2cd5d6715cb1ba6c86b59
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-02-16 03:53:05 +0000

    add upcoming tutorial

--- media/tutorials/index.php
+++ media/tutorials/index.php
@@ -6,6 +6,7 @@ $section='media';?>
 
 <table>
  <tr><th>Date</th><th>Title</th><th>Location</th><th>Presenter</th></tr>
+ <tr><td nowrap>Mac 30 2011</td><td>Introduction to the Cactus Framework</td><td>Loni/HPC tutorial series, CCT (LSU)</td><td>Frank Löffler, Michael Thomas</td></tr>
  <tr><td nowrap>Oct 04 2010</td><td>Introduction to the Cactus Framework</td><td>Loni/HPC tutorial series, CCT (LSU)</td><td>Frank Löffler, Oleg Korobkin</td></tr>
  <tr><td nowrap>Aug 02 2010</td><td>Introduction to the Cactus Framework</td><td>TeraGrid10 conference, student tutorial</td><td>Erik Schnetter</td></tr>
  <tr><td nowrap>Apr 21 2010</td><td>Introduction to the Cactus Framework</td><td>Loni/HPC tutorial series, CCT (LSU)</td><td>Frank Löffler, Tyler Landis</td></tr>
