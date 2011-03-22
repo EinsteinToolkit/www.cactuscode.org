8a9491e38f991dc5f1f94d88d6d8527cb9ab334f
dcastl2 <dcastl2@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-03-22 19:38:47 +0000

    Linked to the most up-to-date GetComponents.

--- download/GetComponents.php
+++ download/GetComponents.php
@@ -2,6 +2,7 @@
 header('Content-type: text/plain');
 #include_once('/var/www/cvs/Utilities/Scripts/GetComponents');
 
-$gcscript = file_get_contents('https://svn.cactuscode.org/Utilities/trunk/Scripts/GetComponents');
+// $gcscript = file_get_contents('https://svn.cactuscode.org/Utilities/trunk/Scripts/GetComponents');
+$gcscript = file_get_contents('https://github.com/gridaphobe/CRL/raw/master/GetComponents');
 echo $gcscript;
 ?>
