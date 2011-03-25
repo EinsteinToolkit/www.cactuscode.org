391da8c06676b2f4e4ee6c63d1dcbe0fb1c76ab5
dcastl2 <dcastl2@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-03-25 17:46:14 +0000

    Cleaned the source files a little.

--- about/team/index.php
+++ about/team/index.php
@@ -12,82 +12,83 @@ team:
 </p>
 
 <h3>Current</h3>
-<ul>
-<li><a href="gallen">Gabrielle Allen</a>
-<br />Lead of Cactus project at <a href="http://www.lsu.edu/">LSU</a> and inactive developer</li>
-<br />
 
-<li><a href="ebentivegna">Eloisa Bentivegna</a>
-<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<ul> <li><a href="gallen">Gabrielle Allen</a>
+<br />Lead of Cactus project at 
+<a href="http://www.lsu.edu/">LSU</a> and inactive developer</li> <br />
 
-<li><a href="rcross">Robert Cross</a>
-<br />Undergraduate student at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="ebentivegna">Eloisa Bentivegna</a>
+<br />Postdoc researcher at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><a href="ljiang">Lei Jiang</a>
-<br />Graduate student in Computer Science at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="rcross">Robert Cross</a>
+<br />Undergraduate student at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><a href="cjordan">Colby Jordan</a>
-<br />Internal Support for Cactus at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="ljiang">Lei Jiang</a>
+<br />Graduate student in Computer Science at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><a href="sko">Soon-Heum Ko</a>
-<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="sko">Soon-Heum Ko</a>
+<br />Postdoc researcher at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><!--<a href="okorobkin">-->Oleg Korobkin<!--</a>-->
-<br />Graduate student at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> and the <a href="http://www.phys.lsu.edu/">Department of Physics &amp; Astronomy</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> Oleg Korobkin
+<br />Graduate student at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> 
+and the 
+<a href="http://www.phys.lsu.edu/">Department of Physics &amp; Astronomy</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><a href="floeffler">Frank Löffler</a>
-<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="floeffler">Frank Löffler</a>
+<br />Postdoc researcher at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><a href="drideout">David Rideout</a>
-<br />Researcher in the <a href="http://www.perimeterinstitute.ca/en/Scientific/Research/Quantum_Foundations/">Quantum  Foundations Group</a> at the <a href="http://www.perimeterinstitute.ca/">Perimeter Institute for Theoretical Physics</a></li>
-<br />
+<li> <a href="drideout">David Rideout</a>
+<br />Researcher in the 
+<a href="http://www.perimeterinstitute.ca/en/Scientific/Research/Quantum_Foundations/"
+>Quantum Foundations Group</a> 
+at the 
+<a href="http://www.perimeterinstitute.ca/">Perimeter Institute for Theoretical Physics</a>
+</li><br />
 
-<li><a href="eschnetter">Erik Schnetter</a>
-<br />Scientist in the <a href="http://www.cct.lsu.edu/numerical/">Numerical Relativity Group</a> at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> and the <a href="http://www.phys.lsu.edu/">Department of Physics &amp; Astronomy</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="eschnetter">Erik Schnetter</a>
+<br />Scientist in the 
+<a href="http://www.cct.lsu.edu/numerical/">Numerical Relativity Group</a> at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> 
+and the 
+<a href="http://www.phys.lsu.edu/">Department of Physics &amp; Astronomy</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
+
+<li> <a href="ericseidel">Eric Seidel</a>
+<br />Undergraduate student in computer science at City College, City University of New York
+</li> <br />
 
 
+<li> <a href="jtao">Jian Tao</a>
+<br />Postdoc researcher at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 
-<li><a href="ericseidel">Eric Seidel</a>
-	   <br />Undergraduate student in computer science at City College, City University of New York</li>
-<br />
-
-
-<li><a href="jtao">Jian Tao</a>
-<br />Postdoc researcher at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
-
-<li><a href="azebrowski">Ashley Zebrowski</a>
-<br />TITLE at the <a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at <a href="http://www.lsu.edu/">LSU</a></li>
-<br />
+<li> <a href="azebrowski">Ashley Zebrowski</a>
+<br />Graduate student at the 
+<a href="http://www.cct.lsu.edu/" title="Center for Computation &amp; Technology">CCT</a> at 
+<a href="http://www.lsu.edu/">LSU</a></li> <br />
 </ul>
 
 <h3>Alumni</h3>
-<ul>
-<li>Thomas Dramlitsch</li>
-<br />
 
-<li>Yaakoub El-Khamra</li>
-<br />
-
-<li>Tom Goodale</li>
-<br />
-
-<li>Gerd Lanfermann</li>
-<br />
-
-<li>Thomas Radke</li>
-<br />
-
-<li>Steve White</li>
-<br />
+ <ul><li>Thomas Dramlitsch
+</li><li>Yaakoub El-Khamra
+</li><li>Tom Goodale
+</li><li>Colby Jordan
+</li><li>Gerd Lanfermann
+</li><li>Thomas Radke
+</li><li>Steve White
 </ul>
 
 <?php
--- documentation/index.php
+++ documentation/index.php
@@ -2,45 +2,43 @@
 <?php $title='Documentation';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
-<p>
 <ul>
 
-  <li>User's Guide (<a href="userguide/UsersGuide.pdf">PDF</a>, 
-  <a href="userguide/UsersGuide.html">HTML</a>) </li>
-    <ul> The User's Guide is for those new to Cactus. It details the
+  <li>User's Guide (<a href="usersguide/UsersGuide.pdf">PDF</a>, 
+  <a href="usersguide/UsersGuide.html">HTML</a>) </li>
+    <ul><p> The User's Guide is for those new to Cactus. It details the
     process of installing and compiling Cactus, running applications, thorn
-    wrting, utility routines, and thorn infrastructure. <br></ul>
+    wrting, utility routines, and thorn infrastructure. <br></ul></p>
   <li>
   Reference Manual (<a href="referencemanual/ReferenceManual.pdf">PDF</a>,
   <a href="referencemanual/ReferenceManual.html">HTML</a>) </li>
-    <ul> The Reference Manual gives a list of thorns, CCTK_* functions, Util_* functions,
+    <ul><p> The Reference Manual gives a list of thorns, CCTK_* functions, Util_* functions,
     and in addition explains configuration file syntax, schedule bins, and CVS commands for
-    checkout and commit procedures. <br></ul>
+    checkout and commit procedures. <br></ul></p>
   <li><a href="CoreDoc.pdf">Advanced Concepts (PDF)</a> </li>
-    <ul> This document concisely explains concepts that span multiple thorns but which
+    <ul><p> This document concisely explains concepts that span multiple thorns but which
     are not part of the flesh, namely tag tables and the post-processing of data from the 
-    CactusEinstein infrastructure. <br></ul> 
+    CactusEinstein infrastructure. <br></ul></p> 
   <li><a href="Readme_Info.php">Overview of Thorn README files </a> </li>
-    <ul> This table gives authors and summaries of the Thorn README files. <br></ul>
+    <ul><p> This table gives authors and summaries of the Thorn README files. <br></ul></p>
   <li><a href="tutorials/">Tutorials</a></li>
-    <ul> These tutorials, some of which were presented in past conferences such as 
+    <ul><p> These tutorials, some of which were presented in past conferences such as 
     TeraGrid, offer an interactive approach to using Cactus; others explain the installation
-    and compilation of Cactus on specific architectures. <br></ul>
+    and compilation of Cactus on specific architectures. <br></ul></p>
   <li><a href="faq">FAQ</a></li>
-    <ul> Frequently Asked Questions regarding the background, CVS checkout, configuration,
-    building, and running of Cactus, in addition to thorn writing and documentation.<br></ul>
+    <ul><p> Frequently Asked Questions regarding the background, CVS checkout, configuration,
+    building, and running of Cactus, in addition to thorn writing and documentation.<br></ul></p>
   <li><a href="arrangements/">Arrangement Guides</a></li>
-    <ul> This includes documentation for the CactusWave and CactusEinstein thorn arrangements.
-    It gives the purpose of the arrangement and the description of the thorns.<br></ul>
+    <ul><p> This includes documentation for the CactusWave and CactusEinstein thorn arrangements.
+    It gives the purpose of the arrangement and the description of the thorns.<br></ul></p>
   <li><a href="thorns/">Thorn Guides</a></li>
-    <ul> This is a list of individual PDF document files detailing the purpose and use of 
-    each thorn.<br></ul>
+    <ul><p> This is a list of individual PDF document files detailing the purpose and use of 
+    each thorn.<br></ul></p>
   <li><a href="visualization/">Visualization</a></li>
-    <ul> This lists and describes visualization packages and data output formats for Cactus.
-    <br></ul>
+    <ul><p> This lists and describes visualization packages and data output formats for Cactus.
+    <br></ul></p>
 
 </ul>
-</p>
 
 <br /><br /><br />
 
