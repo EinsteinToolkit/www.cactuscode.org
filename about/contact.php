27a9687b7a5b8447bdcfdf71c8ac7eff384ed2d7
dcastl2 <dcastl2@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-03-30 17:24:38 +0000

    Changed <code> under /download to use curl and changed WaveDemo.par to generate a number of data sets that can be handled by xgraph.

--- about/contact.php
+++ about/contact.php
@@ -2,12 +2,14 @@
 <?php $title='Contact';
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php'); ?>
 
-<p>We recommend using our <a href="/mailman/listinfo/">Mailing Lists</a> to ask questions about Cactus concerning how to use and develop for it. If you want to contact the <a href="/about/team/">Cactus Team</a> directly, you can mail to <a href="mailto:cactusmaint@cactuscode.org">cactusmaint@cactuscode.org</a>.</p>
+<p> We recommend using our 
+<a href="/community/mailinglists/index.php">Mailing Lists</a> 
+to ask questions about Cactus concerning how to 
+use and develop for it. If you want to contact the 
+<a href="/about/team/">Cactus Team</a> directly, you can mail to 
+<a href="mailto:cactusmaint@cactuscode.org">cactusmaint@cactuscode.org</a>.</p>
 
-<p>We welcome comments and feedback about what you use Cactus for, your experiences with it, and any suggestions for improvement.</p>
+<p>We welcome comments and feedback about what you use Cactus for, your
+experiences with it, and any suggestions for improvement. </p> <br />
 
-<br />
-
-<?php
-include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');
-?>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php'); ?>
