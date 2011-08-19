02f1c40b228ececdfcf03c4e0cde1f71ba06181c
dcastl2 <dcastl2@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-08-19 22:44:08 +0000

    Added article about article in HPCWire.

---
+++ media/news/hpcwire/index.php
@@ -0,0 +1,24 @@
+<?php $title='Cactus Mentioned in HPC Wire Article';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+
+<p> In an article entitled <i>Developing Scientific Computing Communities</i>,
+author Aaron Dubrow cites Cactus as an example of "Modular Software for
+Community Growth." After giving a brief summary of its core capabilities, he
+lauds Cactus as a "successful [model] of community code creation and evolution."</p>
+
+<p> The article quotes Dr. Gabrielle Allen, Cactus co-author and originator, on
+her vision of the Cactus community's aim: "to provide the core computational
+tools than can enable new science, broaden [the] community, facilitate
+interdisciplinary research and take advantage of emerging petascale computers
+and advanced cyberinfrastructure." </p>
+
+<p>
+You may read the electronic article 
+<a href="http://www.hpcwire.com/hpcwire/2011-08-19/developing_scientific_computing_communities.html">
+here</a>.
+</p>
+
+<h3 class="align-right">19 August 2011 — dcastl2</h3>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
