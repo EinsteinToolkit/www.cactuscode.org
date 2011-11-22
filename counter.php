b8752a47354e6c45607f735e834e096ded7a3577
dcastl2 <dcastl2@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2011-11-22 21:53:28 +0000

    Added timestamp and IP information for hits.

--- counter.php
+++ counter.php
@@ -1,11 +1,18 @@
 <?php
+$etime = time();
+$addr = $_SERVER['REMOTE_ADDR'];
+
 $hits = 0;
-$hitfile = fopen("hits", "r");
+$hitfile = fopen("global/hits", "r");
 fscanf($hitfile, "%d", $hits);
 fclose($hitfile);
 
 $hits++;
-$hitfile = fopen("hits", "w");
+$hitfile = fopen("global/hits", "w");
 fprintf($hitfile , $hits);
 fclose($hitfile);
+
+$infofile = fopen("global/hitinfo", "a");
+fprintf($infofile, "%d %s\n", $etime, $addr);
+fclose($infofile);
 ?>
