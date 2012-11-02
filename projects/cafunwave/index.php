8b205565b371e3b8cfa3d1e39623ef51c721f4d0
sbrandt <sbrandt@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2012-11-02 18:11:13 +0000

    first version

--- projects/cafunwave/index.php
+++ projects/cafunwave/index.php
@@ -2,7 +2,28 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <p>
-CaFunwave is...
+CaFunwave is a Thorn which enables simulation of Boussinesq or
+shallow water equations. CaFunwave is based on the
+<a href="http://chinacat.coastal.udel.edu/programs/funwave/funwave.html">Funwave</a> made available by the University of Delaware.
+</p>
+<p>
+<table border="1" cellpadding="5" cellspacing="0">
+<tr>
+<td>
+<img src="images/water.png">
+</td>
+</tr>
+<tr>
+<td>
+<img src="images/bottom.png">
+</td>
+</tr>
+<tr>
+<td>
+<img src="images/funwave-amr.png">
+</tr>
+</td>
+</table>
 </p>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
