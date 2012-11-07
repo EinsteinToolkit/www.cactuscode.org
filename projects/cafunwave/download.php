7310977d1fe6ccd1b69e0784802406910fffced8
sbrandt <sbrandt@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2012-11-07 17:25:39 +0000

    Add download instructions

---
+++ projects/cafunwave/download.php
@@ -0,0 +1,19 @@
+<?php $title='Cactus-related Projects';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
+
+Downloading Funwave + Cactus consists of the following steps:
+
+<pre>
+curl -O https://raw.github.com/gridaphobe/CRL/master/GetComponents
+
+chmod a+x GetComponents
+
+./GetComponents --parallel https://svn.cct.lsu.edu/repos/projects/ngchc/code/CactusCoastal/Funwave/thornlists/funwave_carpet.th
+</pre>
+
+To build, follow the instructions on the
+<a href="http://cactuscode.org">http://cactuscode.org</a> and
+<a href="http://einsteintoolkit.org">http://einsteintoolkit.org</a> 
+websites.
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
