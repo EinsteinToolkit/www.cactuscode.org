15f5f9307429e1b5830401b4d91eafa9fd1ef43f
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2012-11-08 15:42:56 +0000

    add old ET lovelace story

---
+++ media/news/ET_2012_05/index.php
@@ -0,0 +1,10 @@
+<?php $title='';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+<p>
+We are pleased to announce the fifth release (code name "Lovelace") of the Einstein Toolkit, an open, community developed software infrastructure for relativistic astrophysics. The Einstein Toolkit builds, to a large part, on Cactus, plus AMR infrastructure Carpet, the Simfactory tool and a lot of other parts. For details, read the <a href="http://einsteintoolkit.org/about/releases/ET_2012_05_announcement.php">release announcement</a>.
+</p>
+
+<h3 class="align-right">28 May 2012 — knarf</h3>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
