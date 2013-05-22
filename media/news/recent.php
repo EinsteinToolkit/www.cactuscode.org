89c37bf402e04392f6cfbc8000330bc1ac5c8af5
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2013-05-22 18:25:51 +0000

    add stories

--- media/news/recent.php
+++ media/news/recent.php
@@ -1,5 +1,7 @@
-	<li><a href="/media/news/ET_2013_05">Einstein Toolkit "Gauss" release</a></li>
+<p class="align-right"> May 2013
 	<li><a href="/media/news/cactus_4.2.0">Cactus 4.2.0 release</a></li>
+<p class="align-right"> May 2013
+	<li><a href="/media/news/ET_2013_05">Einstein Toolkit "Gauss" release</a></li>
 <p class="align-right"> November 2012
 	<li><a href="/media/news/cactus_4.1.0">Cactus 4.1.0 release</a></li>
 	<li><a href="/media/news/ET_2012_11">Einstein Toolkit "Ørsted" release</a></li>
@@ -8,73 +10,46 @@
 <p class="align-right"> October 2011
 	<li><a href="/media/news/cactus_4.0.0">Cactus 4.0.0 release</a></li>
 	<li><a href="/media/news/ET_2011_10">Einstein Toolkit "Maxwell" release</a></li>
-
 <p class="align-right"> August 2011
 <li><a href="/media/news/hpcwire">Cactus Mentioned in HPC Wire Article</a></li>
-
 <p class="align-right"> July 2011
 </p> <li><a href="/media/news/Mojave2" >Mojave Update</a></li> </a></li>
-
 <p class="align-right"> June 2011
 </p> <li><a href="/media/news/ET_Linux">Einstein Toolkit Featured in Linux Journal</a></li>
 </a></li>
-
 <p class="align-right"> November 2010
-</p><li><a href="/media/news/EinsteinToolkit_2010_11"
->New Einstein Toolkit Release
+</p><li><a href="/media/news/EinsteinToolkit_2010_11">New Einstein Toolkit Release
 </a></li>
-
 <p class="align-right"> June 2010
-</p><li><a href="/media/news/EinsteinToolkit"
->Cactus included in Einstein Toolkit release
+</p><li><a href="/media/news/EinsteinToolkit">Cactus included in Einstein Toolkit release
 </a></li>
-
 <p class="align-right"> April 2010
-</p> <li><a href="/media/news/CVStoSVN">
-Cactus moves from CVS to Subversion
+</p> <li><a href="/media/news/CVStoSVN">Cactus moves from CVS to Subversion
 </a></li>
-
 <p class="align-right"> November 2009
-</p><li><a href="/media/news/Whisky_Retreat_5"
->Whisky Retreat V
+</p><li><a href="/media/news/Whisky_Retreat_5">Whisky Retreat V
 </a></li>
-
 <p class="align-right"> March 2009
-</p><li><a href="/media/news/BH-to-GRB-SiCortex"
->Webcast: <em>From Black Holes to Gamma-Ray Bursts</em>, hosted by SiCortex
+</p><li><a href="/media/news/BH-to-GRB-SiCortex">Webcast: <em>From Black Holes to Gamma-Ray Bursts</em>, hosted by SiCortex
 </a></li>
-
 <p class="align-right">February 2009
-</p><li><a href="/media/news/Cactus4.0beta16-release"
->Cactus 4.0 beta 16 released
+</p><li><a href="/media/news/Cactus4.0beta16-release">Cactus 4.0 beta 16 released
 </a></li>
-
 <p class="align-right">December 2008
-</p><li><a href="/media/news/SC08-Alpaca"
->Cactus team demonstrates Alpaca tools at SuperComputing 2008
+</p><li><a href="/media/news/SC08-Alpaca">Cactus team demonstrates Alpaca tools at SuperComputing 2008
 </a></li>
-
 <p class="align-right">August 2008
-</p><li><a href="/media/news/BGP-131072"
->Cactus runs on 131,072 cores on Blue Gene/P at ANL
+</p><li><a href="/media/news/BGP-131072">Cactus runs on 131,072 cores on Blue Gene/P at ANL
 </a></li>
-
 <p class="align-right">March 2008
-</p><li><a href="/media/news/GPUcomputing"
->CCT researcher accelerates Einstein code with GPU computing
+</p><li><a href="/media/news/GPUcomputing">CCT researcher accelerates Einstein code with GPU computing
 </a></li>
-
 <p class="align-right">Feburary 2008
-</p><li><a href="/media/news/Cactus-Loongson">
-Cactus scalability benchmark results from First Loongson 2F Chinese Cluster
+</p><li><a href="/media/news/Cactus-Loongson">Cactus scalability benchmark results from First Loongson 2F Chinese Cluster
 </a></li>
-
 <p class="align-right">January 2008</p>
-<li><a href="/media/news/XiRelWorkshop08"
->First XiRel Workshop held at LSU
+<li><a href="/media/news/XiRelWorkshop08">First XiRel Workshop held at LSU
 </a></li>
-
 <p class="align-right">January 2008
-</p><li><a href="/media/news/Cactus-Ranger"
->Cactus scalability benchmark results from Ranger at TACC
+</p><li><a href="/media/news/Cactus-Ranger">Cactus scalability benchmark results from Ranger at TACC
 </a></li>
