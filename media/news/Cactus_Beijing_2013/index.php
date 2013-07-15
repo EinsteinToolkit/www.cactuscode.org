8a2f1e1d2e6177847414840fb93d76da740fa8fa
sbrandt <sbrandt@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2013-07-15 17:27:47 +0000

    Modify text and add image.

--- media/news/Cactus_Beijing_2013/index.php
+++ media/news/Cactus_Beijing_2013/index.php
@@ -5,16 +5,17 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 <p>
  "仙人掌：高性能计算开源框架" (Cactus: An open-source framework for
  high-performance computing): That was the title of one of the four invited
- talks Steven R. Brandt and Frank Löffler presented this year in Beijing, China.
+ talks presented by Steven R. Brandt and Frank Löffler this year in Beijing, China.
 </p>
 <p>
- The two leaders of the Cactus project had been traveling to <a
+<div style="float:right;"><img src="china_1.jpg" width="300"></div>
+ The two leaders of the Cactus project travelled to <a
  href="http://www.tsinghua.edu.cn/publish/then/">Tsinghua University</a> to
  visit <a ref="http://hpclab.cs.tsinghua.edu.cn/~duzh/">都志辉 (Zhihui Du)</a>, Associate
  Professor at the Department of Computer Science and Technology.
 </p>
 <p>
- Apart from ... collaboration ... Brandt and Löffler presented Cactus and the
+ Brandt and Löffler presented Cactus and the
  <a href="http://einsteintoolkit.org/">Einstein Toolkit</a> to faculty of the
  <a href="http://www.tsinghua.edu.cn/publish/csen/">Department of Computer
  Science and Technology</a> at Tsinghua University, at the <a
@@ -25,7 +26,9 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
  University.
 </p>
 <p>
- ... Something about the future of this collaboration ...
+ This visit and these talks mark the beginning of a scientific
+ collaboration. We made a joint proposal for computer time on Titan,
+ and are planning a scientific paper in the near future.
 </p>
 
 <h3 class="align-right">day Mon year — user posting</h3>
