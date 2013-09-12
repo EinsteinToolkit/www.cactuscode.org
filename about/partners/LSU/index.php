1f9db1d818181ce8730eaffa9a01cf6c797cba94
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2013-09-12 01:05:28 +0000

    fix link

--- about/partners/LSU/index.php
+++ about/partners/LSU/index.php
@@ -2,8 +2,6 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 
 <p>
-The LSU NumRel group has members at the <a href="http://relativity.phys.lsu.edu/">Department of Physics &amp; Astronomy</a> and the <a href="http://www.cct.lsu.edu/about/focus/numerical/">Center for Computation and Technology</a>.</p>
-
-<p>The group has some private and some public thorns.  These thorns are described in the <a href="http://wiki.cct.lsu.edu/numrel/space/Thorns">wiki</a>.</p>
+The LSU NumRel group has members at the <a href="http://relativity.phys.lsu.edu/">Department of Physics &amp; Astronomy</a> and the <a href="https://www.cct.lsu.edu/focus-areas/coast-cosmos/astrophysics">Center for Computation and Technology</a>.</p>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
