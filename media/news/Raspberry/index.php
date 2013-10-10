d967c436a0c1963b5fef88a392f8d9813e764802
dcastl2 <dcastl2@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2013-10-10 18:03:33 +0000

    Altered the total news release for the Raspberry Pi build.

--- media/news/Raspberry/index.php
+++ media/news/Raspberry/index.php
@@ -6,41 +6,43 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
 
 <p>
 Cactus was ported to run on <a href="http://raspberrypi.org/">Raspberry Pis</a>.
-This isn't a major accomplishment, as these little computers run
-<a href="http://www.raspbian.org/">Raspbian</a>, which is nothing else than
-a costumized <a href="http://www.debian.org">Debian</a>, a wide-spread and
-already supported
-<a href="http://en.wikipedia.org/wiki/Linux_distribution">Linux distribution</a>.
-However, a few tweaks were necessary due to the uncommon architecture
-(a ARMv6 32-bit core), which should be included in the next release of Cactus.
+These credit-card sized computers run
+<a href="http://www.raspbian.org/">Raspbian</a>, a fork of Debian <a 
+href="http://www.debian.org">Debian</a>, a wide-spread and supported
+<a href="http://en.wikipedia.org/wiki/Linux_distribution">Linux 
+distribution</a>.  Owing to the inherent portability of Cactus, the port to 
+Raspberry Pi was straightforward; however a few tweaks were necessary due to the uncommon architecture
+(a ARMv6 32-bit core), which is anticipated to be supported in the next release 
+of Cactus.
 </p>
 
 <p>
-It should also be noted the goal of running on such systems is hardly speed. In
-fact, a lot of modern cell phones are probably faster. However, Raspberry Pis
-are cheap, and a nice tool for education. And of course it is always nice to be
-able to demonstrate how portable Cactus is.
+Since Raspberry Pis are inexpensive and portable hardware, they provide convenient tools
+for education. Though their clock rate is limited to a mere 700 MHz with a current
+maximum supported memory of 512 MB, they nevertheless are able to support
+Cactus runs for small-scale toy codes.  Our next goal is to support
+a distributed setup using Raspberry Pis.
 </p>
 
 <p>
 What has been demonstrated is a built of the
-<a href="http://einsteintoolkit.org/about/releases/ET_2013_05_announcement.php">Gauss</a>
-release of the
+<a href="http://einsteintoolkit.org/about/releases/ET_2013_05_announcement.php">Gauss</a> release of the
 <a href="http://einsteintoolkit.org/">Einstein Toolkit</a>, successfully passing
 all but a few test suites (apparently connected to
 <a href="http://www.hdfgroup.org/HDF5/">HDF5</a> recovery in
 <a href="http://www.carpetcode.org/">Carpet</a>).</p>
 
 <p>
-Building is pretty straightforward: <a href="http://simfactory.org/">Simfactory</a>
-contains an optionlist <a href="https://svn.cct.lsu.edu/repos/numrel/simfactory2/trunk/mdb/optionlists/raspbian.cfg">raspbian.cfg</a>,
-and also lists a couple of packages that, if installed, reduce build-time. The
-only change that is necessary to build the whole Einstein Toolkit on a standard
-Raspbian image (besides the neccessary packages mentioned also in the option list;
-like compilers), is a larger swap space, for compilation of some files (to do this: edit
-<b>/etc/dphys-swapfile</b>, I used 1GB, and then run <b>dphys-swapfile setup</b>
-and <b>dphys-swapfile swapon</b>).
-</p>
+Building is straightforward: <a href="http://simfactory.org/">Simfactory</a>
+now contains an optionlist <a 
+href="https://svn.cct.lsu.edu/repos/numrel/simfactory2/trunk/mdb/optionlists/raspbian.cfg">raspbian.cfg</a>,
+and lists a couple of packages which reduce built-time when installed. Aside 
+from the necessary software included in this optionlist, the
+only change necessary to build Einstein Toolkit on a standard
+Raspbian image is a larger swap space. For the compilation of some files, 1 GB was
+used. (To extend swap space, edit <b>/etc/dphys-swapfile</b>, then run 
+<b>dphys-swapfile setup</b>
+followed by <b>dphys-swapfile swapon</b>).  </p>
 
 <h3 class="align-right">10 October 2013 — knarf</h3>
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
