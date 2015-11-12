f7330229b244802c729a5879330c067b3cc3403d
knarf <knarf@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2015-11-12 15:43:28 +0000

    changes for new release

--- download/index.php
+++ download/index.php
@@ -26,11 +26,10 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 </table>
 
 <h3>Getting the Code</h3>
-<h4>Method #1: GetComponents</h4>
 
 <ul>
 <p> Cactus is maintained using <a
-href="http://subversion.apache.org/">Subversion</a>.  
+href="http://subversion.apache.org/">Subversion</a> and <a href="https://git-scm.com/">git</a>.
 The preferred method is to use the <kbd>GetComponents</kbd> script along with
 a thornlist as described below.  We maintain a 
 <a href="/download/thorns/">page of basic thornlists</a>, 
@@ -38,7 +37,7 @@ but you can use your own customized thornlist.</p>
 
 <p> Download 
 <a href="GetComponents.php">GetComponents</a> and make sure it is executable. 
-<code>wget --no-check-certificate https://raw.github.com/gridaphobe/CRL/master/GetComponents
+<code>wget --no-check-certificate https://raw.github.com/gridaphobe/CRL/Cactus_4.3.0/GetComponents
 chmod a+x GetComponents </code></p>
 
 <p> To checkout the Cactus flesh and required thorns, choose and save one of
@@ -48,46 +47,6 @@ our
 <code>./GetComponents &lt;<var>thornlist-name</var>&gt;</code>
 </ul>
 
-<h4>Method #2: Subversion</h4>
-
-<ul>
-
-<p> Instead of using the <kbd>GetComponents</kbd> script, you can also check
-out Cactus and the Cactus Computational Toolkit using <kbd>svn</kbd> commands
-or another Subversion client.  The main Cactus Subversion Server
-(<kbd>svn.cactuscode.org</kbd>) hosts several different repositories including
-different branches for the stable and development versions of Cactus.  </p>
-
-<p> The Subversion server can be use anonymously to checkout Cactus
-components. It supports both http and https connections, but only https can
-be used for authenticated logins, e.g. for commits to the repositories.  </p>
-
-<p> Check out the Cactus flesh: 
-<code>svn co http://svn.cactuscode.org/flesh/branches/Cactus_4.2.0 Cactus</code> 
-Move to the arrangements directory of the Cactus installation and install the
-required arrangements and thorns: 
-<code>cd Cactus/arrangements</code> 
-If you are using thorns other than those in the standard Cactus distributions
-you will need to know the details for downloading or checking them out. Here
-we just describe checking out from the Cactus distribution.  </p>
-
-<p> To checkout individual thorns (e.g. see our standard <a
-href="/documentation/thorns/">ThornLists</a>):
-
-<code>svn co http://svn.cactuscode.org/arrangements/&lt;<var>arrangement</var>&gt;/&lt;<var>thorn</var>&gt;/branches/Cactus_4.2.0 &lt;<var>thorn</var>&gt;</code>
-</p>
-</ul>
-<!--
-<h4>Method #3: Virtual Machine</h4>
-
-Download VirtualBox <a href="https://www.virtualbox.org/wiki/Downloads">here</a>, then follow the installation instructions.
-
-<code>wget http://www.cct.lsu.edu/~dcastl2/cOS.tgz
-tar -xvvzf http://www.cct.lsu.edu/~dcastl2/cOS.tgz</code>
-
-You may then run VirtualBox. Create a new virtual machine. When prompted whether you want to use a new or existing hard disk, select 'Use Existing Hard Disk',
-then select the CactusOS VirtualBox .vdi file from your VirtualBox VMs folder.
--->
 <h3>License</h3> 
 
 <p> The Cactus Code is (C) Copyrighted by the individual Authors and
---
+++ media/news/cactus_4.3.0/index.php
@@ -0,0 +1,16 @@
+<?php $title='Cactus 4.3.0 release';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+<p>
+We are pleased to announce release 4.3.0 of the Cactus computational toolkit.
+This release includes, among other things, improvements to the Cactus
+flesh (see below for details), new utility thorns, and changed and
+additions to the external library thorns. In addition, bug fixes
+accumulated since the previous release have been included.</p>
+
+<p>Read more about the new release in the longer <a href="http://cactuscode.org/download/releasenotes/Cactus_4.3.0">release notes</a>.</p>
+
+<h3 class="align-right">11 Nov 2015 — Frank Löffler</h3>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
+</p>
