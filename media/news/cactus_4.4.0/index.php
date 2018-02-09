00383317833a230f525458c00338da2b32a59ebd
sbrandt <sbrandt@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2018-02-09 20:04:26 +0000

    The new ET release

---
+++ media/news/cactus_4.4.0/index.php
@@ -0,0 +1,16 @@
+<?php $title='Cactus 4.3.0 release';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+<p>
+We are pleased to announce release 4.4.0 of the Cactus computational toolkit,
+in conjunction with the "<a href="https://en.wikipedia.org/wiki/Nikola_Tesla">Tesla</a>" release of the
+<a href="https://einsteintoolkit.org">Einstein Toolkit</a>.
+This release includes bug fixes and an <a href="https://einsteintoolkit.org/documentation/new-user-tutorial">improved tutorial.</a>
+</p>
+
+<p>Read more about the new release in the longer <a href="https://einsteintoolkit.org/about/releases/ET_2018_02_announcement.html">Einstein Toolkit release notes</a>.</p>
+
+<h3 class="align-right">11 Nov 2015 — Frank Löffler</h3>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
+</p>
