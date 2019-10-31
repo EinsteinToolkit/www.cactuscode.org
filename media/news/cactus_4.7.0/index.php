d74fcf185a9197fedc268072d96065b3ed8874cb
sbrandt <sbrandt@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2019-10-31 18:00:58 +0000

    Switch month

--- media/news/cactus_4.7.0/index.php
+++ media/news/cactus_4.7.0/index.php
@@ -12,6 +12,6 @@ href='https://en.wikipedia.org/wiki/Maria_Goeppert_Mayer'>Proca</a>" release of 
 <p>Read more about the new release in the longer <a
 href="https://einsteintoolkit.org/about/releases/ET_2019_10_announcement.html">Einstein Toolkit release notes</a>.</p>
 
-<h3 class="align-right">Mar 2019 — Steven R. Brandt</h3>
+<h3 class="align-right">Oct 2019 — Steven R. Brandt</h3>
 <?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
 </p>
