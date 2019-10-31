371ba18f3c32f622402d67812e0fe2b6726a09e5
sbrandt <sbrandt@39cfb27f-4e60-0410-80e6-bc70e1efc789>
2019-10-31 17:59:03 +0000

    Add the cactus 4.7 release announcement

---
+++ media/news/cactus_4.7.0/index.php
@@ -0,0 +1,17 @@
+<?php $title='Cactus 4.7.0 release';
+include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');
+?>
+
+<p>
+We are pleased to announce release 4.7.0 of the Cactus computational toolkit
+in conjunction with the "<a
+href='https://en.wikipedia.org/wiki/Maria_Goeppert_Mayer'>Proca</a>" release of the
+<a href="https://einsteintoolkit.org">Einstein Toolkit</a>.
+</p>
+
+<p>Read more about the new release in the longer <a
+href="https://einsteintoolkit.org/about/releases/ET_2019_10_announcement.html">Einstein Toolkit release notes</a>.</p>
+
+<h3 class="align-right">Mar 2019 — Steven R. Brandt</h3>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
+</p>
