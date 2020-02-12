65931031c5ab094d8329911cf11a1729c50db400
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-02-12 12:32:14 +0000

    Remove PHP files that have been successfully converted to markdown

--- global/errors/400.php
+++
@@ -1,6 +0,0 @@
-<?php $title='Error 400';
-include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
-
-<p>Bad Request</p>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
--- global/errors/401.php
+++
@@ -1,6 +0,0 @@
-<?php $title='Unauthorized - Error 403';
-include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
-
-<p>Perhaps retry entering your credentials.</p>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
