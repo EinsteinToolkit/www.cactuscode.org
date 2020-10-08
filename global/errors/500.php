8cf374aedc095ed5ae6368757b46a8d06faff66c
Roland Haas <rhaas@ncsa.illinois.edu>
2020-10-08 16:05:03 -0500

    errors: provide custom error pages for github

--- global/errors/500.php
+++
@@ -1,6 +0,0 @@
-<?php $title='Server Error 500';
-include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
-
-<p>Apologies, there has been an error with the server.</p>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/global/footer.php');?>
